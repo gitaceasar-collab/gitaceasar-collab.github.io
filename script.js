3c048410c447448cc3d211d651079bb71f9927cc
gitaceasar-collab <gitaceasar@gmail.com>
2026-03-03 12:46:46 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-function ubahWarna() {
-    const warna = ["#f4f4f4", "#ffcccc", "#ccffcc", "#ccccff", "#ffffcc"];
-    const random = Math.floor(Math.random() * warna.length);
-    document.body.style.backgroundColor = warna[random];
+function scrollToProjects() {
+    document.getElementById("projects").scrollIntoView({
+        behavior: "smooth"
+    });
 }
 
-function tampilkanPesan() {
-    alert("Halo! Ini adalah pesan dari JavaScript 🚀");
+function kirimPesan() {
+    alert("Terima kasih! Pesan Anda telah dikirim 😊");
 }
